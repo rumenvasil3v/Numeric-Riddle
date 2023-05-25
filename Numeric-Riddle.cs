6e69e8bccfd09511a705236950aa096fe419830a
rumenvasil3v <rumenstvass@gmail.com>
2023-05-25 23:28:38 +0300

    Adding some features

--- Numeric-Riddle.cs
+++ Numeric-Riddle.cs
@@ -18,7 +18,15 @@ namespace Numeric_Riddle_Game_Project
             Console.WriteLine("I give you this only possibility to beat the great mind!");
             Console.WriteLine();
 
+            Console.Write("But before we start, choose your username: ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine($"Okay {username}! Let's go");
+
+            Console.WriteLine($"Choose what difficulty you want to play: ");
             Console.Write("Write your suggestion (1 - 100): ");
+
 
             string playerSuggestions = Console.ReadLine();
             bool isValid = int.TryParse(playerSuggestions, out int playerChoice);
@@ -32,10 +40,7 @@ namespace Numeric_Riddle_Game_Project
                         Console.WriteLine();
                         Console.WriteLine("Mhm...Try again! You went too high!");
                     }
-                }
-                else if (isValid)
-                {
-                    if (computerChoice > playerChoice)
+                    else if (playerChoice < computerChoice)
                     {
                         Console.WriteLine();
                         Console.WriteLine("Mhm...Try again! You went too low!");
@@ -43,11 +48,9 @@ namespace Numeric_Riddle_Game_Project
                 }
                 else
                 {
-                    if (!isValid)
-                    {
-                        Console.WriteLine("Invalid input!");
-                    }
+                    Console.WriteLine("Invalid input!");
                 }
+
                 Console.WriteLine();
                 Console.Write("Write your suggestions (1 - 100): ");
 
@@ -58,7 +61,7 @@ namespace Numeric_Riddle_Game_Project
             Console.WriteLine();
             if (playerChoice == computerChoice)
             {
-                Console.WriteLine(@"I knew it you are great mind, mister!
+                Console.WriteLine(@"I knew it you are a great mind, mister!
 Congrats!");
             }
         }
